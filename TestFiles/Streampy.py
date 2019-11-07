8a1431891565d515e81c85188548392c7dd5bd7a
James Kidd <A00245575@student.ait.ie>
2019-11-07 10:39:38 +0000

    Added CSV export

--- TestFiles/Streampy.py
+++ TestFiles/Streampy.py
@@ -1,32 +1,70 @@
-import json
 import tweepy
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import csv
+from datetime import datetime
 
 consumer_key = "DZC5B3agcSsbGbrI9JQ1xhSCr"
 consumer_secret = "7E0oyANOjc4zdBZ2xsgGCOQtixArmE2zOKdKElcyEJnThJrUQT"
 access_token = "1173508449805754368-v08F2Qskwtz0ntIgLncvGRwXOxrdSl"
 access_token_secret = "VAYw5ovNjoPIoLj3YtIVIaihPVKjgkvEpfaDN2kWbEqiU"
 
-
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+
+def sentiment_scores(sentence):
+    # Create a SentimentIntensityAnalyzer object.
+    sid_obj = SentimentIntensityAnalyzer()
+
+    # polarity_scores method of SentimentIntensityAnalyzer
+    # oject gives a sentiment dictionary.
+    # which contains pos, neg, neu, and compound scores.
+    sentiment_dict = sid_obj.polarity_scores(sentence)
+    return sentiment_dict['compound']
+    # decide sentiment as positive, negative and neutral
+    # if sentiment_dict['compound'] >= 0.05:
+    #     #     return "Positive"
+    #     #
+    #     # elif sentiment_dict['compound'] <= - 0.05:
+    #     #     return "Negative"
+    #     #
+    #     # else:
+    #     #     return "Neutral"
+
+
 class MyStreamListener(tweepy.StreamListener):
     def __init__(self, api):
         self.api = api
         self.me = api.me()
+        row = ["Time", "Sentiment"]
+        with open('sentiment.csv', 'a') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()
 
     def on_status(self, tweet):
+        sentiment = sentiment_scores(tweet.text)
+        now = datetime.now()  # time object
+        row = [now, sentiment]
+        with open('sentiment.csv', 'a') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()
         print(f"{tweet.user.name}:{tweet.text}")
 
     def on_error(self, status):
         print("Error detected")
 
+
 # Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True,
-    wait_on_rate_limit_notify=True)
+                 wait_on_rate_limit_notify=True)
 
 tweets_listener = MyStreamListener(api)
 stream = tweepy.Stream(api.auth, tweets_listener)
-stream.filter(track=["DnD"], languages=["en"])
+stream.filter(track=["Trump"], languages=["en"])
+
+
+
